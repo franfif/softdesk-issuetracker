b1f2012e2a72576026264d749f9945b80164abd4
franfif <francis.barrow@gmail.com>
2023-04-20 21:24:44 -0400

    add issues ProjectView

--- issues/views.py
+++ issues/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+from issues.serializers import ProjectSerializer
+
+from issues.models import Project
+
+
+class ProjectViewSet(APIView):
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        # if admin, return all projects
+        # else, return user's projects
+        return Project.objects.filter(contributors=self.request.user)
