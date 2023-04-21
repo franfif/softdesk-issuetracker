537e6ea8dec9008a2f0988e9a51cabc01445a136
franfif <francis.barrow@gmail.com>
2023-04-20 21:23:35 -0400

    add issues serializers

---
+++ issues/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+
+from .models import Project, ProjectContributor, Issue, Comment
+from rest_framework import serializers
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'title', 'description', 'type', 'contributors']
+
+    def create(self, validated_data):
+        owner = get_user_model()
+
+        return Project.objects.create(**validated_data)
+
+
+class ProjectContributorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectContributor
+        fields = ['id', 'project', 'contributor', 'role']
+
+
+class IssueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = ['id', 'title', 'description', 'tag', 'priority',
+                  'project', 'status', 'author', 'assignee', 'created_time']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        field = ['description', 'author', 'project', 'created_time']
