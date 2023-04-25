f7f5fa424148a76c5de1c3a8cdfc4b9504a952f9
franfif <francis.barrow@gmail.com>
2023-04-25 10:35:57 -0400

    rearrange INSTALLED_APPS

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "authentication",
     "issues",
-    "rest_framework",
 ]
 
 MIDDLEWARE = [
