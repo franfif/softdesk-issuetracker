0cbf2a0ccac891aead3290b5a3525f7405928987
franfif <francis.barrow@gmail.com>
2023-04-28 14:18:45 -0400

    add and comment out ViewSets

--- issues/views.py
+++ issues/views.py
@@ -20,7 +20,32 @@ class ProjectViewSet(ModelViewSet):
         print('user is logged in', self.request.user)
         return Project.objects.filter(contributors=self.request.user)
 
-    def perform_create(self, serializer):
-        project = serializer.save()
-        project.contributors.add(self.request.user,
-                                 through_defaults={'role': 'Owner'})
+# class ProjectViewSet(ModelViewSet):
+#     serializer_class = ProjectSerializer
+#
+#     # permission_classes = [IsAuthenticated]
+#
+#     def get_queryset(self):
+#         # if admin, return all projects
+#         # else, return user's projects
+#         if self.request.user.is_staff:
+#             return Project.objects.all()
+#         # For now, return all projects if user is not authenticated
+#         # return Project.objects.filter(contributors=self.request.user)
+#         return Project.objects.all()
+#
+#     def perform_create(self, serializer):
+#         project = serializer.save()
+#         # For now, allow creation if user is not authenticated
+#         # project.contributors.add(self.request.user,
+#         #                          through_defaults={'role': 'Owner'})
+
+
+# class ContributorViewSet(ModelViewSet):
+#     serializer_class = ContributorSerializer
+#     queryset = Contributor.objects.all()
+#
+#     def get_queryset(self, pk=None):
+#         if pk is not None:
+#             return ProjectContributor.objects.filter(project=pk)
+#         return ProjectContributor.objects.all()
