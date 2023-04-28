4744582a207f418107231b1b04185cd33750efc4
franfif <francis.barrow@gmail.com>
2023-04-28 17:04:37 -0400

    update serializer to debug

--- issues/serializers.py
+++ issues/serializers.py
@@ -7,38 +7,58 @@ from rest_framework import serializers
 class ContributorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contributor
-        fields = ['user', 'role']
+        fields = ['user']
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-    # contributors = ContributorSerializer(many=True)
-    # cont = serializers.SerializerMethodField()
+    # contributors = serializers.SerializerMethodField()
+    contributors = ContributorSerializer(many=True)
 
     class Meta:
         model = Project
-        # fields = ['id', 'title', 'description', 'type', 'owner', 'contributors']
-        fields = ['id', 'title', 'description', 'type', 'owner']
+        fields = ['id', 'title', 'description', 'type', 'owner', 'contributors']
+        # fields = ['id', 'title', 'description', 'type', 'owner']
 
-    # def get_cont(self, instance):
-    #     queryset = instance,
+    # def get_contributors(self, instance):
+    #     queryset = instance.contributors.all()
+    #     serializer = ContributorSerializer(queryset, many=True)
+    #     return serializer.data
 
-    # def create(self, validated_data):
-    #     contributors = validated_data.pop('contributors')
-    #     project = Project.objects.create(**validated_data)
-    #     contributor_ids = []
-    #     for contributor in contributors:
-    #         contributor_ids.append(contributor.id)
-    #
-    #     project.contributors.set(contributor_ids)
-    #     return project
-    #
-    # def update(self, instance, validated_data):
-    #     contributors = validated_data.pop('contributors')
-    #     project = super().update(instance, **validated_data)
-    #     for contributor in contributors:
-    #         project.contributors.add(contributor.contributor,
-    #                                  through_default={'role': 'Contributor'})
-    #     return project
+    def create(self, validated_data):
+        # get request in the serializer from context
+        print('Project create - validated_data')
+        print(validated_data)
+        contributors_data = validated_data.pop('contributors')
+        # project = super().create(validated_data)
+        project = Project.objects.create(**validated_data)
+
+        request = self.context.get('request')
+        owner = request.user
+        project.contributors.add(Contributor.objects.create(user=owner,
+                                                            role=Contributor.OWNER,
+                                                            project=project))
+        # contributor_ids = []
+        for contributor_data in contributors_data:
+            print('Project create - contributor_data')
+            print(contributor_data)
+            Contributor.objects.create(project=project, role=Contributor.CONTRIBUTOR, **contributor_data)
+
+        # project.contributors.set(contributor_ids)
+        return project
+
+    def update(self, instance, validated_data):
+        print('Project update - validated_data')
+        print(validated_data)
+        print('instance')
+        print(instance)
+        contributors_data = validated_data.pop('contributors')
+        project = Project.objects.update(instance, **validated_data)
+        for contributor_data in contributors_data:
+            print(contributor_data)
+            Contributor.objects.create(project=project,
+                                       role=Contributor.CONTRIBUTOR,
+                                       **contributor_data)
+        return project
 
     # def create(self, validated_data):
     #     # get request in the serializer from context
