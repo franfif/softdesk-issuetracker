22cc772b9033cea46d8abd08bd0a594e7f927209
franfif <francis.barrow@gmail.com>
2023-05-02 12:03:50 -0400

    remove contributor access from project view

--- issues/serializers.py
+++ issues/serializers.py
@@ -10,7 +10,7 @@ class ContributorSerializer(serializers.ModelSerializer):
 
 class ProjectSerializer(serializers.ModelSerializer):
     # contributors = serializers.SerializerMethodField()
-    contributors = ContributorSerializer(many=True)
+    contributors = ContributorSerializer(many=True, read_only=True)
 
     class Meta:
         model = Project
@@ -42,23 +42,23 @@ class ProjectSerializer(serializers.ModelSerializer):
         return project
 
     def update(self, instance, validated_data):
-        contributors_data = validated_data.pop('contributors')
+        # contributors_data = validated_data.pop('contributors')
         project = super().update(instance, validated_data)
 
-        # Convert contributors_data to a list of users
-        new_contributors = list(map(lambda c: c['user'], contributors_data))
-        # Convert the existing contributors of this project to a list of users
-        existing_contributors = list(map(lambda c: c.user, Contributor.objects.filter(project=project)))
-
-        # Create new contributors if the user is not already a contributor
-        for user in (user for user in new_contributors if user not in existing_contributors):
-            Contributor.objects.create(project=project,
-                                       role=Contributor.CONTRIBUTOR,
-                                       user=user)
-        # Remove contributors who are not in the updated data
-        for user in (user for user in existing_contributors if user not in new_contributors):
-            # TO DO? Do we want to prevent the suppression of the contributor if they are the project's owner?
-            Contributor.objects.get(project=project, user=user).delete()
+        # # Convert contributors_data to a list of users
+        # new_contributors = list(map(lambda c: c['user'], contributors_data))
+        # # Convert the existing contributors of this project to a list of users
+        # existing_contributors = list(map(lambda c: c.user, Contributor.objects.filter(project=project)))
+        #
+        # # Create new contributors if the user is not already a contributor
+        # for user in (user for user in new_contributors if user not in existing_contributors):
+        #     Contributor.objects.create(project=project,
+        #                                role=Contributor.CONTRIBUTOR,
+        #                                user=user)
+        # # Remove contributors who are not in the updated data
+        # for user in (user for user in existing_contributors if user not in new_contributors):
+        #     # TO DO? Do we want to prevent the suppression of the contributor if they are the project's owner?
+        #     Contributor.objects.get(project=project, user=user).delete()
 
         return project
 
