856486156cf5431fd53556599dbe3499fcb506b4
franfif <francis.barrow@gmail.com>
2023-05-04 15:50:36 -0400

    register Issue and Comment to admin site

--- issues/admin.py
+++ issues/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Issue, Comment
 
 admin.site.register(Project)
+admin.site.register(Issue)
+admin.site.register(Comment)
