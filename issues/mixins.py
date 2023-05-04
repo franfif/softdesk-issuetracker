4b06f63fc607fb9b45e27512b4adf5f2ff6108f3
franfif <francis.barrow@gmail.com>
2023-05-04 14:37:43 -0400

    add mixins to get queryset or get object in views

--- issues/mixins.py
+++ issues/mixins.py
@@ -1,4 +1,7 @@
-class ProductQuerySetMixin():
+from rest_framework import generics
+
+
+class ProductQuerySetMixin:
     contributors_field = 'member'
     allow_staff_view = True
 
@@ -11,3 +14,36 @@ class ProductQuerySetMixin():
         if self.allow_staff_view and user.is_staff:
             return qs
         return qs.filter(**lookup_data)
+
+
+class MultipleFieldListViewMixin:
+    """
+    Apply this mixin to any List view to get multiple field filtering
+    queryset based on a `lookup_fields` attribute,
+    instead of the default single field filtering.
+    """
+    def get_queryset(self, *args, **kwargs):
+        filter_ = {}
+        for field in self.lookup_fields:
+            if self.kwargs.get(field):
+                filter_[field] = self.kwargs[field]
+        obj = self.queryset.filter(**filter_)
+        # Check permissions ?
+        return obj
+
+
+class MultipleFieldDetailViewMixin:
+    """
+    Apply this mixin to any Detail view to get multiple field filtering
+    object based on a `lookup_fields` attribute,
+    instead of the default single field filtering.
+    """
+    def get_object(self):
+        queryset = self.get_queryset()     # Get the base queryset
+        filter_ = {}
+        for field in self.lookup_fields:
+            if self.kwargs.get(field):     # Ignore empty fields.
+                filter_[field] = self.kwargs[field]
+        obj = generics.get_object_or_404(queryset, **filter_)  # Lookup the object
+        # self.check_object_permissions(self.request, obj)     # /!\ Add permissions first
+        return obj
