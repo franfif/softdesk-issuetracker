e0a421b117094ccd170e6163b351860557bea696
franfif <francis.barrow@gmail.com>
2023-05-08 15:17:50 -0400

    add lifetime to jwt tokens

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from datetime import timedelta
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,4 +135,9 @@ REST_FRAMEWORK = {
         # 'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),  # minutes=5
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),  # days=1
 }
