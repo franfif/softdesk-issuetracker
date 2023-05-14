08a02697323ba35f98e1dfb23c2f0535056b9ca6
franfif <francis.barrow@gmail.com>
2023-05-14 12:13:25 -0400

    override create method of UserSerializer to encrypt password

--- issuetracker/urls.py
+++ issuetracker/urls.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+import authentication.views
 from issues import views
 
 
@@ -13,12 +14,16 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 
+    path('signup/',
+         authentication.views.UserCreate.as_view(),
+         name="signup"),
     path('token/',
          TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('token/refresh/',
          TokenRefreshView.as_view(),
          name='token_refresh'),
+
     path('projects/',
          views.ProjectListAPIView.as_view(),
          name='project-list'),
