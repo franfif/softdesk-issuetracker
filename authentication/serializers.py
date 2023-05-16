17d93203804df13853be1df448b2d400a3eb49cc
franfif <francis.barrow@gmail.com>
2023-05-16 09:40:56 -0400

    add user password validation

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
 from rest_framework import serializers
 
 from .models import User
@@ -12,6 +15,10 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         password = validated_data.pop('password')
         user = User(**validated_data)
+        try:
+            validate_password(password=password, user=user)
+        except ValidationError as err:
+            raise serializers.ValidationError({'password': err.messages})
         user.set_password(password)
         user.save()
         return user
