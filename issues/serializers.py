238f0a79bcbd304572e98b9c82569d4d6b7a3edb
franfif <francis.barrow@gmail.com>
2023-05-24 17:25:55 -0400

    refactor issue and comment serializers to show certain fields depending on request

--- issues/serializers.py
+++ issues/serializers.py
@@ -3,27 +3,13 @@ from rest_framework import serializers
 from .models import Project, Contributor, Issue, Comment
 
 
-class ContributorSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Contributor
-        fields = ['id', 'user', 'role', 'project']
-        read_only_fields = ['user', 'role', 'project']
-
-    def get_user(self, obj):
-        user = {'id': obj.user.id,
-                'name': obj.user.username}
-        return user
-
-
+# Project Serializers
 class ProjectListSerializer(serializers.ModelSerializer):
     owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
         fields = ['id', 'title', 'type', 'owner']
-        read_only_fields = ['owner']
 
     def get_owner(self, obj):
         return obj.owner.username
@@ -66,7 +52,39 @@ class ProjectDetailSerializer(serializers.ModelSerializer):
         return issues
 
 
-class IssueSerializer(serializers.ModelSerializer):
+# Contributor Serializers
+class ContributorSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Contributor
+        fields = ['id', 'user', 'role', 'project']
+        read_only_fields = ['user', 'role', 'project']
+
+    def get_user(self, obj):
+        user = {'id': obj.user.id,
+                'name': obj.user.username}
+        return user
+
+
+# Issue Serializers
+class IssueListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = ['id', 'title', 'tag', 'priority', 'status']
+
+
+class IssueCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = ['id', 'title', 'description', 'tag', 'priority',
+                  'project', 'status', 'author', 'assignee', 'created_time']
+        read_only_fields = ['project', 'created_time', 'author']
+
+
+class IssueDetailSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+    assignee = serializers.SerializerMethodField()
     comments = serializers.SerializerMethodField()
 
     class Meta:
@@ -75,17 +93,54 @@ class IssueSerializer(serializers.ModelSerializer):
                   'project', 'status', 'author', 'assignee', 'created_time', 'comments']
         read_only_fields = ['project', 'created_time', 'author']
 
+    def get_author(self, obj):
+        return {'id': obj.author.id,
+                'username': obj.author.username}
+
+    def get_assignee(self, obj):
+        return {'id': obj.assignee.id,
+                'username': obj.assignee.username}
+
     def get_comments(self, obj):
         comments = []
         for comment in obj.comment_set.all():
             comments.append({'id': comment.id,
-                             'description': comment.description,
-                             'author': comment.author.username})
+                             'description': comment.description})
         return comments
 
 
-class CommentSerializer(serializers.ModelSerializer):
+# Comment Serializers
+class CommentListSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'description', 'author']
+
+    def get_author(self, obj):
+        return obj.author.username
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'description', 'author', 'issue', 'created_time']
         read_only_fields = ['issue', 'created_time', 'author']
+
+
+class CommentDetailSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+    issue = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'description', 'author', 'issue', 'created_time']
+        read_only_fields = ['issue', 'created_time', 'author']
+
+    def get_author(self, obj):
+        return {'id': obj.author.id,
+                'username': obj.author.username}
+
+    def get_issue(self, obj):
+        return {'id': obj.issue.id,
+                'title': obj.issue.title}
--- issues/views.py
+++ issues/views.py
@@ -7,7 +7,8 @@ from . import serializers
 from .models import Project, Issue, Comment, Contributor
 
 
-class ProjectListAPIView(
+# Project Views
+class ProjectListCreateAPIView(
         generics.ListCreateAPIView):
     # Any authenticated user should be able to create a project
     permission_classes = [permissions.IsAuthenticated]
@@ -22,7 +23,6 @@ class ProjectListAPIView(
         queryset = Project.objects.filter(contributors__user=self.request.user)
         return queryset
 
-    # perform_create() from the view or create() from the serializer?
     def perform_create(self, serializer):
         owner = self.request.user
         project = serializer.save(owner=owner)
@@ -50,104 +50,8 @@ class ProjectDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
         return obj
 
 
-class IssueListAPIView(generics.ListCreateAPIView):
-    serializer_class = serializers.IssueSerializer
-    # Only contributors should be able to create & read issues on a project
-    permission_classes = [permissions.IsAuthenticated,
-                          IsProjectContributor]
-
-    def get_queryset(self):
-        return Issue.objects.filter(project=self.kwargs.get('project_id'))
-
-    def perform_create(self, serializer):
-        project = generics.get_object_or_404(Project,
-                                             id=self.kwargs.get('project_id'))
-
-        if 'assignee' in serializer.validated_data:
-            assignee = serializer.validated_data.pop('assignee')
-            print(assignee)
-            # if assignee is not a contributor in project
-            # raise ValidationError
-            if not Project.objects.filter(id=self.kwargs.get('project_id'),
-                                          contributors__user=assignee).exists():
-                raise serializers.ValidationError(
-                    {'assignee': "The issue's assignee must be a contributor of this project."})
-        else:
-            assignee = self.request.user
-
-        serializer.save(project=project,
-                        author=self.request.user,
-                        assignee=assignee)
-
-
-class IssueDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = serializers.IssueSerializer
-    # Only the author should be able to update & delete an issue
-    permission_classes = [permissions.IsAuthenticated,
-                          IsProjectContributor,
-                          IsAuthorOrReadOnly]
-
-    def get_queryset(self):
-        queryset = Issue.objects.filter(project=self.kwargs.get('project_id'))
-        return queryset
-
-    def get_object(self):
-        obj = generics.get_object_or_404(self.get_queryset(),
-                                         id=self.kwargs.get('id'))
-        return obj
-
-    def perform_update(self, serializer):
-        if 'assignee' in serializer.validated_data:
-            assignee = serializer.validated_data.pop('assignee')
-            # if assignee is not a contributor in project
-            # raise ValidationError
-            if not Project.objects.filter(id=self.kwargs.get('project_id'),
-                                          contributors__user=assignee).exists():
-                raise serializers.ValidationError(
-                    {'assignee': "The issue's assignee must be a contributor of this project."})
-        else:
-            assignee = self.request.user
-
-        serializer.save(assignee=assignee)
-
-
-class CommentListAPIView(generics.ListCreateAPIView):
-    serializer_class = serializers.CommentSerializer
-    # Only contributors should be able to create & read comments on an issue
-    permission_classes = [permissions.IsAuthenticated,
-                          IsProjectContributor]
-
-    def get_queryset(self):
-        queryset = Comment.objects.filter(issue__project=self.kwargs.get('project_id'),
-                                          issue=self.kwargs.get('issue_id'))
-        return queryset
-
-    def perform_create(self, serializer):
-        issue = generics.get_object_or_404(Issue,
-                                           id=self.kwargs.get('issue_id'))
-        serializer.save(issue=issue,
-                        author=self.request.user)
-
-
-class CommentDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = serializers.CommentSerializer
-    # Only the author should be able to update & delete a comment
-    permission_classes = [permissions.IsAuthenticated,
-                          IsProjectContributor,
-                          IsAuthorOrReadOnly]
-
-    def get_queryset(self):
-        queryset = Comment.objects.filter(issue__project=self.kwargs.get('project_id'),
-                                          issue=self.kwargs.get('issue_id'))
-        return queryset
-
-    def get_object(self):
-        obj = generics.get_object_or_404(self.get_queryset(),
-                                         id=self.kwargs.get('id'))
-        return obj
-
-
-class ContributorListAPIView(generics.ListCreateAPIView):
+# Contributor Views
+class ContributorListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = serializers.ContributorSerializer
     # Only the project owner should be able to add contributors
     permission_classes = [permissions.IsAuthenticated,
@@ -180,4 +84,111 @@ class ContributorDestroyAPIView(generics.DestroyAPIView):
     def get_object(self):
         obj = generics.get_object_or_404(self.get_queryset(),
                                          user=self.kwargs.get('user_id'))
+        return obj
+
+
+# Issue Views
+class IssueListCreateAPIView(generics.ListCreateAPIView):
+    # Only contributors should be able to create & read issues on a project
+    permission_classes = [permissions.IsAuthenticated,
+                          IsProjectContributor]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return serializers.IssueCreateSerializer
+        return serializers.IssueListSerializer
+
+    def get_queryset(self):
+        return Issue.objects.filter(project=self.kwargs.get('project_id'))
+
+    def perform_create(self, serializer):
+        project = generics.get_object_or_404(Project,
+                                             id=self.kwargs.get('project_id'))
+
+        if 'assignee' in serializer.validated_data:
+            assignee = serializer.validated_data.pop('assignee')
+            print(assignee)
+            # if assignee is not a contributor in project
+            # raise ValidationError
+            if not Project.objects.filter(id=self.kwargs.get('project_id'),
+                                          contributors__user=assignee).exists():
+                raise serializers.ValidationError(
+                    {'assignee': "The issue's assignee must be a contributor of this project."})
+        else:
+            assignee = self.request.user
+
+        serializer.save(project=project,
+                        author=self.request.user,
+                        assignee=assignee)
+
+
+class IssueDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.IssueDetailSerializer
+    # Only the author should be able to update & delete an issue
+    permission_classes = [permissions.IsAuthenticated,
+                          IsProjectContributor,
+                          IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        queryset = Issue.objects.filter(project=self.kwargs.get('project_id'))
+        return queryset
+
+    def get_object(self):
+        obj = generics.get_object_or_404(self.get_queryset(),
+                                         id=self.kwargs.get('id'))
+        return obj
+
+    def perform_update(self, serializer):
+        if 'assignee' in serializer.validated_data:
+            assignee = serializer.validated_data.pop('assignee')
+            # if assignee is not a contributor in project
+            # raise ValidationError
+            if not Project.objects.filter(id=self.kwargs.get('project_id'),
+                                          contributors__user=assignee).exists():
+                raise serializers.ValidationError(
+                    {'assignee': "The issue's assignee must be a contributor of this project."})
+        else:
+            assignee = self.request.user
+
+        serializer.save(assignee=assignee)
+
+
+# Comment Views
+class CommentListCreateAPIView(generics.ListCreateAPIView):
+    # Only contributors should be able to create & read comments on an issue
+    permission_classes = [permissions.IsAuthenticated,
+                          IsProjectContributor]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return serializers.CommentCreateSerializer
+        return serializers.CommentListSerializer
+
+    def get_queryset(self):
+        queryset = Comment.objects.filter(issue__project=self.kwargs.get('project_id'),
+                                          issue=self.kwargs.get('issue_id'))
+        return queryset
+
+    def perform_create(self, serializer):
+        issue = generics.get_object_or_404(Issue,
+                                           id=self.kwargs.get('issue_id'))
+        serializer.save(issue=issue,
+                        author=self.request.user)
+
+
+class CommentDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.CommentDetailSerializer
+    # Only the author should be able to update & delete a comment
+    permission_classes = [permissions.IsAuthenticated,
+                          IsProjectContributor,
+                          IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        queryset = Comment.objects.filter(issue__project=self.kwargs.get('project_id'),
+                                          issue=self.kwargs.get('issue_id'))
+        return queryset
+
+    def get_object(self):
+        obj = generics.get_object_or_404(self.get_queryset(),
+                                         id=self.kwargs.get('id'))
         return obj
